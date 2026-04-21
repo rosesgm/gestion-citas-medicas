7ecfe69ff22f2778662a15d9f1dc37c313a4c5c5
rosagabriela <rosagabrielapina@gmail.com>
2026-04-21 15:37:04 -0700

    Se creó DisponibilidadService junto con un método estaDisponible

---
+++ src/main/java/com/citasmedicas/service/DisponibilidadService.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.citasmedicas.service;
+
+import com.citasmedicas.model.Medico;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+/**
+ *
+ * @author rosagabriela
+ */
+public class DisponibilidadService {
+    public boolean estaDisponible(Medico medico, LocalDate fecha, LocalTime hora){
+    
+        return true;
+    }
+}
