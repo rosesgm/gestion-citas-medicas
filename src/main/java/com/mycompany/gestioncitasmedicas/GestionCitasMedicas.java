032f42ec6a9f3c2cac22a0e9436b601bc77a2c45
rosagabriela <rosagabrielapina@gmail.com>
2026-04-21 13:42:57 -0700

    commit inicial

---
+++ src/main/java/com/mycompany/gestioncitasmedicas/GestionCitasMedicas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gestioncitasmedicas;
+
+/**
+ *
+ * @author rosagabriela
+ */
+public class GestionCitasMedicas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
