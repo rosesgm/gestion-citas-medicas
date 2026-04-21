64ebf13f478c3300a727956c2b5e2ad5a507e209
rosagabriela <rosagabrielapina@gmail.com>
2026-04-21 14:10:26 -0700

    changed package name

--- src/main/java/com/gestioncitasmedicas/GestionCitasMedicas.java
+++ src/main/java/com/gestioncitasmedicas/GestionCitasMedicas.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.gestioncitasmedicas;
+package com.gestioncitasmedicas;
 
 /**
  *
