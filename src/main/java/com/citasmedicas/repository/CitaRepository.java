202f3c9a38f485b9145247274df45aeea356bc61
rosagabriela <rosagabrielapina@gmail.com>
2026-04-21 16:06:55 -0700

    Updated CitaRepository

---
+++ src/main/java/com/citasmedicas/repository/CitaRepository.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.citasmedicas.repository;
+
+import com.citasmedicas.model.Cita;
+
+/**
+ *
+ * @author rosagabriela
+ */
+public class CitaRepository {
+    public void guardar(Cita cita){
+    System.out.println("Cita guardada correctamente");
+}
+   
+}
